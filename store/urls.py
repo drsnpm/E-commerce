1ca9b0750b8673e79cf5a99d16e08105fe317071
Darshan Parameshwar Moger <130597128+drsnpm@users.noreply.github.com>
2025-03-01 11:29:08 +0530

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Index, SignUp, Login, Logout, Cart, CheckOut, OrderView, Profile, Updateprofile, UpdatePassword, delete_order, remove_from_cart, Payment
+from .views import Index, SignUp, Login, Logout, Cart, CheckOut, OrderView, Profile, Updateprofile, UpdatePassword, delete_order, remove_from_cart
 
 urlpatterns = [
     path('',Index.as_view(), name='homepage'),
@@ -8,7 +8,7 @@ urlpatterns = [
     path('logout', Logout, name='logout'),
     path('cart', Cart.as_view(), name='cart'),
     path('checkout', CheckOut.as_view(), name='checkout'),
-    path('payment', Payment.as_view(), name='payment'),
+    # path('payment', Payment.as_view(), name='payment'),
     path('orders', OrderView.as_view(), name='orders'),
     path('profile',Profile.as_view(), name='profile'),
     path('updateprofile', Updateprofile.as_view(), name='updateprofile'),
